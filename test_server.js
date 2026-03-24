74057fdaeed191e886c8b438f1b017aca6d82c7f
Md Rezawl <mdrezawl@Mds-MacBook-Pro.local>
2026-03-24 12:37:33 +0600

    Refine logo slider spacing and tighten section gaps

---
+++ test_server.js
@@ -0,0 +1,9 @@
+import http from 'http';
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello, World!');
+});
+server.listen(5173, '127.0.0.1', () => {
+  console.log('Server running on http://127.0.0.1:5173/');
+  process.exit(0);
+});
